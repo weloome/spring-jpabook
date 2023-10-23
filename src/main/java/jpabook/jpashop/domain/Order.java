5ccedd0470b1a6679bc300c00c30be75411d1b97
danbi <leedanbiii@naver.com>
2023-10-23 23:40:29 +0900

    엔티티 연관관계 편의 메서드 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -33,4 +33,25 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    /*
+    * 연관관계 편의 메서드
+    * - 보통 양방향 연관관계에서 객체 간의 연관관계를 설정하기 위해 사용
+    * - 한 쪽의 변경을 다른 쪽에도 자동으로 반영해주는 역할
+    * */
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
