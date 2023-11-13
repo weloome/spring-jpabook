63201593da03e0d61d5c340b4b83173f35bce04e
danbi <leedanbiii@naver.com>
2023-11-14 00:57:19 +0900

    orderItems 포함 주문 조회: 페치 조인 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -27,6 +27,15 @@ public class OrderApiController {
         return collect;
     }
 
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+        return collect;
+    }
+
     @Data
     static class OrderDto {
         private Long orderId;
